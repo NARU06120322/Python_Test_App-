138c16c96773f1bfb9e126aa6c1d1e202209a2f7
NARU06120322 <junnarurei@gmail.com>
2025-06-25 22:50:53 +0900

    編集

---
+++ example.py
@@ -0,0 +1,18 @@
+print("hello world")
+
+def greet():
+    print("こんにちは")
+greet()
+
+def print_name(name):
+    print("私の名前は" + name + "です")
+print_name("naru")
+
+def get_greet():
+    return("おはようございます")
+print(get_greet())
+
+def add(a,b):
+    return a + b
+x=add(1,2)
+print(x)
